3f36cdc55e3122b57745d3ee0ba46ef125501b2d
Suborno <112712723+Suborno22@users.noreply.github.com>
2024-08-01 12:58:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from diabetes_predictor import predict_diabetes
 
@@ -23,4 +24,5 @@ def predict():
     return jsonify({'result': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
