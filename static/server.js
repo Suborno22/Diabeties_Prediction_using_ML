d8f3cdabfd12c320fc9fe443b7d2680ad20e6c23
Suborno22 <subornodas251@gmail.com>
2025-07-04 19:31:58 +0530

    Adam's Apple

---
+++ static/server.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    async function AdamS_apple(params) {
+        
+    }
+    window.onload(AdamS_apple)
+// fetch('https://api.ipify.org?format=json')
+// .then(Response=>Response.json())
+// .then( data=>{ console.log(data.ip)})
+})
